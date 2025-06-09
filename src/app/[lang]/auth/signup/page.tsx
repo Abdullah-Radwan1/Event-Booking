3b698504afad467c4ac87af0cb011600bb08cff6
abdullah <abdallahbeedo855@gmail.com>
2025-06-09 22:58:35 +0300

    removing unused redirect

--- src/app/[lang]/auth/signup/page.tsx
+++ src/app/[lang]/auth/signup/page.tsx
@@ -17,7 +17,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { redirect, useParams, useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 const SignUp = () => {
